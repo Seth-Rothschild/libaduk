ac710312e491257559392995ebda947c4a6d3522
Seth Rothschild <seth.j.rothschild@gmail.com>
2026-03-24 03:54:59 -0400

    fix: catch file not found errors if they make it past nginx

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ import { connectDb } from './src/lib/server/db.js';
 import { handler } from './build/handler.js';
 import { attachWebSocketServer } from './src/lib/server/rooms.js';
 
+process.on('uncaughtException', (err) => {
+  if (err.code === 'ENOENT' && err.syscall === 'open') {
+    console.error(`Missing file: ${err.path}`);
+    return;
+  }
+  console.error('Uncaught exception:', err);
+  process.exit(1);
+});
+
 const port = process.env.PORT ?? 3000;
 
 await connectDb();
