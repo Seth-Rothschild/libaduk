11e3fb5b9fe8145007220d939ad7604a33a8224e
Seth Rothschild <seth.j.rothschild@gmail.com>
2026-05-18 05:18:02 -0400

    fix: cancel game for first two moves

--- src/routes/play/[gameId]/+page.svelte
+++ src/routes/play/[gameId]/+page.svelte
@@ -1180,7 +1180,7 @@
           }}>{t('Memorize')}</button
         >
       {:else}
-        {#if !isSpectator && gs.status === 'waiting'}
+        {#if !isSpectator && (gs.status === 'waiting' || (gs.status === 'playing' && gs.totalPly < 2))}
           <button class="button button-red" onclick={cancel}>{t('Cancel Game')}</button>
         {:else if !isSpectator && gs.status === 'playing'}
           {#if pendingMove && boardSettings.moveConfirmation === 'button'}
